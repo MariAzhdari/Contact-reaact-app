886d6e074e14ad3e09a4651b3d63b93e5ebade10
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-17 01:57:53 +0100

    .

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Mo Nahvi",
+    imgURL:
+      "https://media.licdn.com/dms/image/D4E03AQG7yc1MshPnSA/profile-displayphoto-shrink_100_100/0/1669587102407?e=1687392000&v=beta&t=TLET5N_ry3roOirf38cVqMyRmE2ckpDKbrfUD_-FoE4",
+    phone: "+123 456 789",
+    email: "monahvi@gmail.com",
+  },
+  {
+    name: "Marziyeh Azhdari",
+    imgURL:
+      "https://media.licdn.com/dms/image/C5603AQFf4BiSX-5G0A/profile-displayphoto-shrink_200_200/0/1658686743622?e=1687392000&v=beta&t=WeYWRx7RrUhXvCXlk-j9kdG3p17o-reX7U-PHTX_qRE",
+    phone: "+987 654 321",
+    email: "Mrai.gmail.com",
+  },
+  {
+    name: "Eli Mortazavi",
+    imgURL:
+      "https://media.licdn.com/dms/image/C4E03AQGp7zq_ZYG0Rg/profile-displayphoto-shrink_100_100/0/1658870166027?e=1687392000&v=beta&t=5UKI_K0IJEepMzk2YZzYmbDkHdMjE5isE4pgeOurK5Y",
+    phone: "+918 372 574",
+    email: "gmail@Eli.com",
+  },
+];
+
+export default contacts;
