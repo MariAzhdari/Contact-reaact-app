02a375c1ed2ca460412c920e10309b8f327c3acd
Mari Azh <Marzi.aghchay@gmail.com>
2023-04-20 12:46:59 +0100

    maping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,27 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function creatcard(contact){
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar avatarImage="https://media.istockphoto.com/id/1411766919/photo/the-concept-of-using-technology-to-search-for-information-a-mans-hand-is-using-a-smartphone.jpg?b=1&s=170667a&w=0&k=20&c=XcJNceAFLl8kx_d_LaC5sRCv_gjwqx4ZslX-El8SnS0=" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+
+      {contacts.map(creatcard)}
+    
     </div>
   );
 }
